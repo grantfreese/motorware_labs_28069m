edbc47c812e4dc0c09da9ef8d298370cf0a6a4de
Grant Freese <grant@lime-lab.com>
2015-07-08 16:28:08 -0700

    Removed build directory

--- foc_proj_lab02c/Flash/drivers/user.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-drivers/user.obj: C:/ti/motorware/motorware_1_01_00_14/sw/modules/user/src/32b/user.c
-drivers/user.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.4/include/math.h
-drivers/user.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.4/include/linkage.h
-
-C:/ti/motorware/motorware_1_01_00_14/sw/modules/user/src/32b/user.c: 
-C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.4/include/math.h: 
-C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.4/include/linkage.h: 
